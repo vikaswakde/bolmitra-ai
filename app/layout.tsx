f7eff123d1b212f9a94025e5be18c525665d5fe1
vikaswakde <vikaswakdepc@gmail.com>
2025-01-19 21:06:37 +0530

    layout overlow x hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
       <html lang="en">
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+            "min-h-screen bg-background font-sans antialiased overflow-x-hidden",
             fontSans.variable
           )}
         >
